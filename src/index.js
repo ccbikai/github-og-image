2e43f0a5a59cb7968a0a41fc9b2f45db4e0cda8b
ccbikai <ccbikai@gmail.com>
2023-12-19 12:04:33 +0800

    feat: v1 done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env, context) {
+		// 读取缓存
+		const cacheUrl = new URL(request.url);
+		const cacheKey = new Request(cacheUrl.toString());
+		const cache = caches.default;
+		const hasCache = await cache.match(cacheKey);
+		if (hasCache) {
+			console.log('cache: true');
+			return hasCache;
+		}
+		const githubUrl = `https://github.com${cacheUrl.pathname}`;
+		console.log(githubUrl);
+		const githubRes = await fetch(githubUrl);
+		let githubImage = 'https://github.githubassets.com/assets/campaign-social-031d6161fa10.png';
+		const html = new HTMLRewriter()
+			.on('meta[property="og:image"]', {
+				element: (element) => {
+					githubImage = element.getAttribute('content');
+					return element;
+				},
+			})
+			.transform(githubRes);
+		await html.text();
+		const imageRes = await fetch(githubImage, {
+			headers: {
+				'user-agent': request.headers.get('user-agent'),
+			},
+		});
+		const imageResClone = new Response(imageRes.body, {
+			headers: {
+				'cache-control': 'public, max-age=43200',
+			},
+		});
+		// 写入缓存
+		context.waitUntil(cache.put(cacheKey, imageResClone.clone()));
+		return imageResClone;
 	},
 };
